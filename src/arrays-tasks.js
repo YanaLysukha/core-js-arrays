935bd34648b30a8780569df9266ea090510b1fdc
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-04 13:09:16 +0100

    feat: implement get indices of odd numbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -438,8 +438,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((acc, number) => {
+    const index = numbers.indexOf(number);
+    if (number % 2 === 1) acc.push(index);
+    return acc;
+  }, []);
 }
 
 /**
