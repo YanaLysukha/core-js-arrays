5fad184444dcf5c5a2f2aea588afe6e795a28234
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-04 12:51:14 +0100

    feat: implement get element by indexes function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -377,8 +377,10 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  if (indices.length === 1) return arr[indices[0]];
+  const currentInd = indices[0];
+  return getElementByIndices(arr[currentInd], indices.slice(1));
 }
 
 /**
