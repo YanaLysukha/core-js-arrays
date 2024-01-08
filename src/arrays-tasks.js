646e70ddc0574db2e67a85dfc8b060af19e05648
YanaLysukha <yanina.lysukha@gmail.com>
2024-01-08 18:07:35 +0100

    feat: implement shift array function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -565,8 +565,15 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n > 0) {
+    const firstPart = arr.slice(arr.length - n);
+    const secondPart = arr.slice(0, arr.length - n);
+    return firstPart.concat(secondPart);
+  }
+  const firstPart = arr.slice(Math.abs(n));
+  const secondPart = arr.slice(0, Math.abs(n));
+  return firstPart.concat(secondPart);
 }
 
 /**
